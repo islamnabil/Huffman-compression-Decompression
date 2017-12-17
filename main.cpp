c90e25210080ee1e37a9886e4e94652510014f74
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-17 21:44:12 +0200

    made main take command line args.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,35 @@ void test_encoding();
 
 void test_decoding();
 
-int main() {
-    test_encoding();
+int main(int argc, char **argv) {
+    string file_name;
+    if (argc == 2) {
+        cout << "Compressing..\n";
+        file_name = argv[1];
+        My_File my_file = read_file(file_name);
+        Huffman huffman(my_file.freq);
+        string compressed = huffman.encode(my_file.content);
+        write_encoded_file(file_name, compressed, huffman.get_codes());
+    } else if (argc == 3) {
+        string flag = argv[1];
+        if (flag != "-x") {
+            cout << "invalid params only -x is valid\n";
+            exit(1);
+        }
+        cout << "Extracting..\n";
+        file_name = argv[2];
+        My_File my_file = read_encoded_file(file_name);
+        Huffman huffman(my_file.code);
+        vector<int> original_file = huffman.decode(my_file.content);
+        write_file("src.txt.decoded.", original_file);
+    } else {
+        cout << "invalid params\nCompress file -> ./huff [file name]\nExtract file  -> ./huff -x [file name]\n\n";
+        exit(1);
+    }
 
-    test_decoding();
-
+//    test_encoding();
+//
+//    test_decoding();
     return 0;
 }
 
@@ -30,5 +54,5 @@ void test_decoding() {
 
     vector<int> original_file = huffman.decode(my_file.content);
 
-    write_file("src.txt1.", original_file);
+    write_file("src.txt.decoded.", original_file);
 }
